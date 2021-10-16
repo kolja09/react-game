9abedfc3be15076fb3b68770c74d9f53068fdc5a
kolja09 <kolja9062005@gmail.com>
2021-10-16 17:59:10 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './styles.scss'
 import Header from "./components/Header";
-import { Route, Switch } from "react-router-dom";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
 import Play from "./components/Play";
 import Game from "./components/Game";
 import Footer from "./components/Footer";
@@ -11,18 +11,20 @@ const App = () => {
     const [score, setScore] = useState(0)
     return (
         <>
-            <div className='container'>
-                <Header score={score}/>
-                <Switch>
-                    <Route exact path='/'>
-                        <Play setMyChoice={setMyChoice}/>
-                    </Route>
-                    <Route path='/game'>
-                        <Game myChoice={myChoice} score={score} setScore={setScore}/>
-                    </Route>
-                </Switch>
-            </div>
-            <Footer />
+            <Router>
+                <div className='container'>
+                    <Header score={score}/>
+                    <Switch>
+                        <Route exact path='/'>
+                            <Play setMyChoice={setMyChoice}/>
+                        </Route>
+                        <Route path='/game'>
+                            <Game myChoice={myChoice} score={score} setScore={setScore}/>
+                        </Route>
+                    </Switch>
+                </div>
+                <Footer />
+            </Router>
         </>
     );
 };
